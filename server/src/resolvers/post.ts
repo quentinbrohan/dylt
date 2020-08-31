b9111e11865863c781a868fac1cd40c2fb1bd940
QuentinBrohan <brohan.quentin@gmail.com>
2020-09-01 00:27:54 +0200

    add register resolver

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx, Int, Arg, Mutation } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
 
@@ -11,7 +11,7 @@ export class PostResolver {
 
     @Query(() => Post, { nullable: true })
     post(
-        @Arg('id', () => Int) id: number,
+        @Arg('id') id: number,
         @Ctx() { em }: MyContext
         ): Promise<Post | null> {
             return em.findOne(Post, { id });
