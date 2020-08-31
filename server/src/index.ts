9ad5beefff227e8d3a47c4cc28a91bbade0ad00f
QuentinBrohan <brohan.quentin@gmail.com>
2020-08-31 22:50:17 +0200

    setup MikroORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hell-O');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Posts';
+import microConfig from './mikro-orm.config';
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+    // const post = orm.em.create(Post, { title: 'my first post' });
+    // await orm.em.persistAndFlush(post);
+
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
+};
+
+main().catch((error) => {
+    console.log(error)
+});
